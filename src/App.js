b3f687a8443b376e57bf13efcd27e360c01d6ce0
mediacloner <alexbcn@gmail.com>
2018-02-15 12:40:10 +0100

    update bussines logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './img/logo.svg';
-import player1 from './img/player1.svg';
-import player2 from './img/player2.svg';
-import people from './img/people.svg';
-import planetHearth from './img/planetHearth.svg';
-import './App.css';
-import { Jumbotron } from 'reactstrap';
-import gamePlay from './components/gamePlay'
-import countDown from './components/countDown'
-import initialization from './components/initialization'
-
+import React, { Component } from "react";
+import logo from "./img/logo.svg";
+import player1 from "./img/player1.svg";
+import player2 from "./img/player2.svg";
+import people from "./img/people.svg";
+import planetHearth from "./img/planetHearth.svg";
+import "./App.css";
+import { Jumbotron } from "reactstrap";
 
 //import BaseMap from "./components/Maps/BaseMap";
 import apiCountries from "./Helpers/ApiCountries";
 
-"use strict";
+("use strict");
 // Settings
 const countDownDefault = 100000;
 const maxTimeGuessSeconds = 12;
 const countriesIterations = 10;
-
+const extraBonus = 2000;
 
 class App extends Component {
   constructor() {
@@ -31,6 +27,7 @@ class App extends Component {
       score1: 0,
       score2: 0,
       countDown: countDownDefault,
+      substractCountDown: false,
       selectorCountries: [],
       countriesRawResults: [],
       currentCountry: {
@@ -53,7 +50,10 @@ class App extends Component {
     };
   }
 
+  //////////////////////////////Behaviour Button/////////////////////////////
+
   actionButton() {
+    //Action Button
     if (
       this.state.selectorCountries.length <
       this.state.currentCountry.posSelecCountries
@@ -64,6 +64,7 @@ class App extends Component {
       this.currentCountry(this.state.currentCountry.posSelecCountries + 1); // Next Country
       this.countDown(1); // Start CounDown
     } else if (
+      // Last Screen
       (this.state.selectorCountries.length = this.state.currentCountry.posSelecCountries)
     ) {
       this.setState({ currentPage: "FinalScreen" });
@@ -71,14 +72,24 @@ class App extends Component {
     }
   }
 
+  //////////////////////////////Game Play/////////////////////////////
+
+  setSubstractCountDown = val => {
+    if (val === true) {
+      this.setState({ substractCountDown: true });
+      console.log("Sub True");
+    } else this.setState({ substractCountDown: false });
+  };
+
   componentWillMount() {
-    this.areYouRight();
+    let clock = setInterval(this.countDown, 500);
+
+    // MOVE LOGIC
     if (typeof this.countriesRawResults === "undefined") {
       //Inicialization
       this.retrieveCountries();
-    }
-
-    if (
+      this.randomFocusPlayer();
+    } else if (
       this.state.tryAnswer === this.state.currentCountry.population &&
       this.state.screen === "GameScreen"
     ) {
@@ -86,16 +97,170 @@ class App extends Component {
       this.countDown(0);
       this.setState({ win: 1 });
       this.serState({ messages: "You guess the population" });
-      addToScore(this.state.focusPlayer);
+      this.addToScore(this.state.focusPlayer);
       this.setState({ enableOK: true });
+    } else if (
+      this.state.tryAnswer != this.state.currentCountry.population &&
+      this.state.screen === "GameScreen"
+    ) {
+      /// Wrong attemp
+      this.serState({ messages: "You guess the population" });
+      this.changePlayer();
+    } else if (
+      this.state.countDown === 0 &&
+      this.state.screen === "GameScreen"
+    ) {
+      /// Wrong attemp
+      this.serState({
+        messages: "Ups, You don't have people to take for your planet"
+      });
+      this.changePlayer();
     }
+  }
 
-      if (this.state.tryAnswer != this.state.currentCountry.population && 
-        this.state.screen === "GameScreen") {
-        /// Wrong attemp
-        this.serState({ messages: "You guess the population" });
-        this.changePlayer();
+  addToScoreBonus(player) {
+    if (player === 1) {
+      this.setState(prevState => {
+        score1: prevState.score1 + extraBonus;
+      });
+    } else {
+      this.setState(prevState => {
+        score2: prevState.score2 + extraBonus;
+      });
+    }
+    this.setState; //Reset Count Down
+  }
+
+  // Updating the Score.
+  addToScore(player) {
+    if (player === 1) {
+      this.setState(prevState => {
+        score1: prevState.score1 + this.state.countDown;
+      });
+    } else {
+      this.setState(prevState => {
+        score2: prevState.score2 + this.state.countDown;
+      });
+    }
+  }
+
+  extraBonus = (capital, region) => {
+    if (capital === this.state.currentCountry.capital) {
+      this.addToScoreBonus(this.state.focusPlayer);
+      this.setState({ messages: "You are right whith the capital " });
+    } else this.setState({ messages: "You are wrong whith the capital " });
+
+    if (region === this.state.currentCountry.subRegion) {
+      this.addToScoreBonus(this.state.focusPlayer);
+      this.setState(prevState => {
+        messages: prevState.messages + "and you know the region";
+      });
+    } else {
+      this.setState(prevState => {
+        messages: prevState.messages + "and you don't know the region";
+      });
+    }
+  };
+
+  // Field the object of country that are playing at this momment
+
+  currentCountry(posArrRandom) {
+    // start with 0, 1, 2, 3...
+    let posRawData = this.state.selectorCountries[posArrRandom];
+
+    this.setState({
+      currentCountry: {
+        posSelecCountries: posArrRandom,
+        name: this.state.countriesRawResults[posRawData].name,
+        capital: this.state.countriesRawResults[posRawData].capital,
+        subRegion: this.state.countriesRawResults[posRawData].subRegion,
+        population:
+          Math.round(
+            this.state.countriesRawResults[posRawData].population / 100000
+          ) / 10,
+        flag: this.state.countriesRawResults[posRawData].flag,
+        latlng: this.state.countriesRawResults[posRawData].latlng,
+        alpha3Code: this.state.countriesRawResults[posRawData].alpha3Code
       }
+    });
+  }
+
+  changePlayer() {
+    if (this.state.focusPlayer === 1) this.setState({ focusPlayer: 2 });
+    else this.setState({ focusPlayer: 1 });
+  }
+
+  //////////////////////////////Count Down/////////////////////////////
+
+  countDown = () => {
+    let substractFraction = Math.round(
+      countDownDefault / (maxTimeGuessSeconds * 2)
+    );
+    console.log("clock");
+
+    if (this.state.substractCountDown === true) {
+      if (this.state.countDown <= 0) {
+        this.setState({ substractCountDown: false });
+        this.setState({ countDown: 0 });
+      } else {
+        this.setState(prevState => {
+          return { countDown: prevState.countDown - substractFraction };
+        });
+      }
+    }
+  };
+
+  //////////////////////////////iniciatization/////////////////////////////
+
+  randomFocusPlayer = () => {
+    this.setState({ focusPlayer: Math.round(Math.random()) + 1 });
+  };
+
+  retrieveCountries() {
+    apiCountries
+      .searchAllCountries()
+      .then(countries => this.setState({ countriesRawResults: countries }))
+      .then(() =>
+        this.randomArrNumber(
+          countriesIterations,
+          this.state.countriesRawResults.length
+        )
+      )
+      .then(() =>
+        this.currentCountry(this.state.currentCountry.posSelecCountries)
+      );
+  }
+
+  // (Number of iterations, Maximun Random Number that you can modify in top settings )
+
+  randomArrNumber(iterArr, maxRandomNum) {
+    let arrResult = [];
+
+    for (let i = 0; i <= iterArr - 1; i++) {
+      let loop = 0;
+      let randomNum;
+      while (loop === 0) {
+        randomNum = Math.floor(Math.random() * maxRandomNum);
+        if (arrResult.indexOf(randomNum) < 0) loop = 1;
+      }
+      arrResult.push(randomNum);
+    }
+    this.setState({ selectorCountries: arrResult });
+  }
+
+  // Insert the name of the players.
+  setPlayers = (player1, player2) => {
+    this.setState({ player1: player1, player2: player2 });
+  };
+
+  // Field opcions extra points of Guess the region.
+  subRegionField() {
+    let arrSubRegions = [];
+    this.countriesRawResults.forEach(e => {
+      if (arrSubRegions.indexOf(e.subRegion) < 0)
+        arrSubRegions.push(e.subRegion.sort); // Filter for no duplicate options
+    });
+    this.setState({ subRegions: arrSubRegions.sort() });
   }
 
   changePage = page => {
@@ -116,7 +281,7 @@ class App extends Component {
           <PlayerScreen
             changePage={this.changePage}
             setPlayers={this.setPlayers}
-            countDown={this.countDown}
+            setSubstractCountDown={this.setSubstractCountDown}
           />
         )}
         {currentPage === "GameScreen" && (
@@ -128,134 +293,172 @@ class App extends Component {
   }
 }
 
-
-class SplashScreen extends Component{
-
+class SplashScreen extends Component {
   handleClick = () => {
-    this.props.changePage("PlayerScreen")
+    this.props.changePage("PlayerScreen");
     //this.props.callAPI()
-  }
+  };
 
-  render(){
+  render() {
     return (
       <Jumbotron>
-        <img src={logo} className="img-fluid" alt="Responsive image"/>
+        <img src={logo} className="img-fluid" alt="Responsive image" />
         <div>
           <h5>Do you want to fill your planet?</h5>
-          <p>Show your knowledge about earths population, beat your opponent, and fill up your planet with some population!</p>
-          <button type="button" className="btn btn-success" onClick={this.handleClick}>Set Players</button>
+          <p>
+            Show your knowledge about earths population, beat your opponent, and
+            fill up your planet with some population!
+          </p>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.handleClick}
+          >
+            Set Players
+          </button>
         </div>
       </Jumbotron>
-    )
+    );
   }
 }
 
-class PlayerScreen extends Component{
+class PlayerScreen extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const player1 = e.target.elements.player1.value;
+    const player2 = e.target.elements.player2.value;
+    this.props.setPlayers(player1, player2);
+    this.props.changePage("GameScreen");
+    this.props.setSubstractCountDown(true); //TODO:   Counter
+    setTimeout(() => {
+      this.props.setSubstractCountDown(false);
+      console.log("input reset");
+    }, 5000);
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const player1 = e.target.elements.player1.value
-    const player2 = e.target.elements.player2.value
-    this.props.setPlayers(player1, player2)
-    this.props.changePage('GameScreen')
-    this.props.countDown(1)
-    setTimeout(
-      this.props.countDown(0)
-    , 5000);
-
-
-  }
-
-  render(){
+  render() {
     return (
-    <Jumbotron>
-      <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label>Planet 1</label>
-          <div className="row justify-content-center">
-            <input type="text" className="form-control col-sm-5" placeholder="Insert your name" name="player1" required/>
+      <Jumbotron>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Planet 1</label>
+            <div className="row justify-content-center">
+              <input
+                type="text"
+                className="form-control col-sm-5"
+                placeholder="Insert your name"
+                name="player1"
+                required
+              />
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label>Planet 2</label>
-          <div className="row justify-content-center">
-            <input type="text" className="form-control col-sm-5" placeholder="Insert your name" name="player2" required/>
+          <div className="form-group">
+            <label>Planet 2</label>
+            <div className="row justify-content-center">
+              <input
+                type="text"
+                className="form-control col-sm-5"
+                placeholder="Insert your name"
+                name="player2"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-success">
+              Start Game
+            </button>
           </div>
-          <button type="submit" className="btn btn-success">Start Game</button>
-        </div>
-      </form>
-    </Jumbotron>
-    )
+        </form>
+      </Jumbotron>
+    );
   }
 }
 
-class GameScreen extends Component{
-
+class GameScreen extends Component {
   handleSubmit = () => {
-    this.props.changePage('FinalScreen')
-  }
+    this.props.changePage("FinalScreen");
+  };
 
-  render(){
+  render() {
     return (
       <Jumbotron>
         <div className="display-4">
           <div>
-            <Counter peopleCounter={this.props.countDown}/>
+            <Counter peopleCounter={this.props.countDown} />
           </div>
         </div>
         <div className="container">
           <div className="row">
+            <div className="col-sm">One of three columns</div>
             <div className="col-sm">
-              One of three columns
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Insert your name"
+              />
             </div>
             <div className="col-sm">
-              <input type="text" className="form-control" placeholder="Insert your name"/>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Insert your name"
+              />
             </div>
-            <div className="col-sm">
-              <input type="text" className="form-control" placeholder="Insert your name"/>
-            </div>
-            <div className="col-sm">
-              One of three columns
-            </div>
+            <div className="col-sm">One of three columns</div>
           </div>
         </div>
       </Jumbotron>
-    )
+    );
   }
 }
 
-function Counter (props){
-
+function Counter(props) {
   return (
-  <div>
-    <img src={planetHearth} className="img-fluid justify-content-center" alt="Responsive image"/>
-    <div><h3>{props.peopleCounter}</h3></div>
-    <img src={people} className="img-fluid justify-content-center" alt="Responsive image"/>
-  </div>
-  )
+    <div>
+      <img
+        src={planetHearth}
+        className="img-fluid justify-content-center"
+        alt="Responsive image"
+      />
+      <div>
+        <h3>{props.peopleCounter}</h3>
+      </div>
+      <img
+        src={people}
+        className="img-fluid justify-content-center"
+        alt="Responsive image"
+      />
+    </div>
+  );
 }
 
-function FinalScreen (props){
-
+function FinalScreen(props) {
   return (
-  <Jumbotron>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm">
-          <img src={player1} className="img-fluid justify-content-center" alt="Responsive image"/>
-          One of three columns
-        </div>
-        <div className="col-sm">
-          <input type="text" className="form-control" placeholder="Insert your name"/>
-        </div>
-        <div className="col-sm">
-          <img src={player2} className="img-fluid" alt="Responsive image"/>
-          One of three columns
+    <Jumbotron>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <img
+              src={player1}
+              className="img-fluid justify-content-center"
+              alt="Responsive image"
+            />
+            One of three columns
+          </div>
+          <div className="col-sm">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Insert your name"
+            />
+          </div>
+          <div className="col-sm">
+            <img src={player2} className="img-fluid" alt="Responsive image" />
+            One of three columns
+          </div>
         </div>
       </div>
-    </div>
-  </Jumbotron>
-  )
+    </Jumbotron>
+  );
 }
 
 export default App;
